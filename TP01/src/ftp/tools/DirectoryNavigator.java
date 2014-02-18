5ba79edda844e915721c40075d675b737141a3f4
decima <h.larget@gmail.com>
2014-02-18 23:16:48 +0100

    J'ai écris les commentaires dans ma classe, je n'arrive pas à séparer le code avec les commandes, vu qu'on a pas mal d'objets qui dépendent de la classe FTPRequest, notamment le directory. Si quelqu'un a une idée.

--- TP01/src/ftp/tools/DirectoryNavigator.java
+++ TP01/src/ftp/tools/DirectoryNavigator.java
@@ -4,39 +4,56 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-
+/**
+ * Classe de gestion de la navigation à travers les répertoires.
+ * @author Groupe 4 équipe 1.
+ */
 public class DirectoryNavigator {
 
-	private String working_directory;
-	private String real_directory;
+    private String working_directory;
+    private String real_directory;
 
-	public DirectoryNavigator(String real_directory) {
-		this.real_directory = real_directory;
-		this.working_directory = File.separator;
-	}
-
-	public String get_absolute_path_to_working_directory() throws IOException {
-		File f = new File(this.real_directory + File.separator
-				+ this.working_directory + File.separator);
-		return f.getCanonicalPath();
-	}
-
-	public String get_working_directory() {
-		return this.working_directory;
-	}
-
-    public void go_upper_directory() throws IOException {
-            change_working_directory(".." + File.separator);
-
+    public DirectoryNavigator(String real_directory) {
+        this.real_directory = real_directory;
+        this.working_directory = File.separator;
+    }
+/**
+ *  Permet d'obtenir le chemin absolu par rapport au répertoire / du système.
+ * @return Le chemin absolu.
+ * @throws IOException  dans le cas ou le chemin n'est pas trouvé.
+ */
+    public String get_absolute_path_to_working_directory() throws IOException {
+        File f = new File(this.real_directory + File.separator
+                + this.working_directory + File.separator);
+        return f.getCanonicalPath();
     }
 
+    public String get_working_directory() {
+        return this.working_directory;
+    }
+/**
+ * Permet de remonter d'un répertoire.
+ * @throws IOException le dossier supperieur n'existe pas.
+ */
+    public void go_upper_directory() throws IOException {
+        change_working_directory(".." + File.separator);
+
+    }
+/**
+ * Permet de changer le répertoire courant pour le répertoire passé en paramètres.
+ * @param path le nouveau chemin, relatif ou absolu vers le dossier souhaité.
+ * @throws IOException le répertoire n'est pas accessible.
+ */
     public void change_working_directory(String path) throws IOException {
         String head = this.calculate_absolute_path(File.separator);
         String absolute = this.calculate_absolute_path(path);
         this.working_directory = absolute.replace(head, "");
         this.working_directory += File.separator;
     }
-
+/**
+ * Permet de retourner au répertoire racine du serveur FTP.
+ * @return le succès de la tache; Il renvoie faux dans le seul cas ou le répertoire du serveur FTP n'est plus accessible.
+ */
     public Boolean change_working_directory() {
         try {
             this.change_working_directory(File.separator);
@@ -45,11 +62,17 @@ public class DirectoryNavigator {
             return false;
         }
     }
-
-    public String[] list_working_directory(String path) throws IOException, NullPointerException {
-        System.err.println(calculate_absolute_path(path));
+/**
+ * Récupère la liste du répertoire passé en paramètre.
+ * @param path le répertoire à lister
+ * @return la liste des fichiers.
+ * @throws IOException si un fichier/dossier n'est pas accessible.
+ * @throws NullPointerException si des informations sont vide
+ */
+    public String[] list_working_directory(String path) throws IOException {
         File folder = new File(this.calculate_absolute_path(path));
-        List<String> s = new ArrayList<>();
+        List<String> s = new ArrayList<String>();// indispensable sous Java 6, déclanche un warning sous Java 7.
+        //sous Java 7, on peut utiliser la syntaxe : List<String> s = new ArrayList<>(); pour éviter la redondance.
         File[] listOfFiles = new File[]{};
         if (folder.isDirectory()) {
             listOfFiles = folder.listFiles();
@@ -72,8 +95,13 @@ public class DirectoryNavigator {
         s.toArray(simple);
         return simple;
     }
-
-    public String calculate_absolute_path(String path) throws IOException, NullPointerException {
+/**
+ * Calcul du chemin absolu du dossier
+ * @param path le chemin à calculer
+ * @return une chaine de caractère contenant le chemin canonique du fichier
+ * @throws IOException Si le chemin n'existe pas, ou n'est pas dans le répertoire racine du serveur FTP.
+ */
+    public String calculate_absolute_path(String path) throws IOException {
         File f = new File(this.real_directory + File.separator);
         String s = f.getCanonicalPath();
         File file = new File(s + File.separator + path);
@@ -88,7 +116,7 @@ public class DirectoryNavigator {
         throw new IOException("wrong path");
     }
 
-	public String[] list_working_directory() throws IOException {
-		return this.list_working_directory(this.working_directory);
-	}
+    public String[] list_working_directory() throws IOException {
+        return this.list_working_directory(this.working_directory);
+    }
 }
